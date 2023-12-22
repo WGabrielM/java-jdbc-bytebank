56b58f78cd08ff717fea2ff6a4709d3ea1623049
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-12-22 08:46:42 -0300

    add connectionFactory

--- src/main/java/br/com/alura/bytebank/ConectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConectionFactory.java
@@ -4,16 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoDB {
-    public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=1234");
-            System.out.println("Recuperei a conexão");
+public class ConectionFactory {
 
-            connection.close();
+    public Connection recuperarConexao() {
+        try {
+            return DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=1234");
         } catch(SQLException e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
     }
 }
