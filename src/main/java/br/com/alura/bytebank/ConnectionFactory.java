b163507ac23062d755c87ac344365f718db1ce1c
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-12-22 11:16:23 -0300

    use preparedStatement and insert data on db

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConectionFactory {
+public class ConnectionFactory {
 
     public Connection recuperarConexao() {
         try {
